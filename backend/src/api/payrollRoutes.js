18d3f35ae2b979678c5c58b13a21f655b89d425a
fnenow <fnenow@gmail.com>
2025-05-31 15:02:29 -0700

    Delete backend directory

--- backend/src/api/payrollRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const payrollController = require('../controllers/payrollController');
-// const { protect, admin } = require('../middleware/authMiddleware'); // Future
-
-router.get('/report', /* admin, */ payrollController.generatePayrollReport);
-router.put('/time-entries/:entryId/billing', /* admin, */ payrollController.updateBillingInfo);
-
-module.exports = router;
