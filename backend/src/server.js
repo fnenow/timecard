0f4a789fea11c00242f2c298b61eaa46b4c7b622
fnenow <fnenow@gmail.com>
2025-06-02 12:43:59 -0700

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,6 @@
 //v2
 const app = require('./app');
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;  //original is 3001
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
