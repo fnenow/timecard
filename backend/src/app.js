707b0ba0b72cf1a1d90fe596b57560928410b35f
fnenow <fnenow@gmail.com>
2025-06-02 22:03:47 -0700

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,14 +3,13 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 app.use(cors());
+// Middleware, routes, etc.
+app.use(express.json());
 
 
 const sequelize = require('./models/index');
 const workerRoutes = require('./api/workerRoutes');
 app.use('/api/workers', workerRoutes);
-
-// Middleware, routes, etc.
-app.use(express.json());
 
 // Example worker route (add others as needed)
 const workerController = require('./controllers/workerController');
